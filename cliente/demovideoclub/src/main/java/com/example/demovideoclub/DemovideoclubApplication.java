d794ab91c32fd82676f9049d0f9fb40e503e2afb
AlejandroSoftwareOne <ext.alejandro.munoz@softwareone.com>
2022-11-14 21:21:34 +0100

    FP DUAL SANTIAGO

---
+++ cliente/demovideoclub/src/main/java/com/example/demovideoclub/DemovideoclubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demovideoclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemovideoclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemovideoclubApplication.class, args);
+	}
+
+}
---
+++ cliente/demovideoclub/src/test/java/com/example/demovideoclub/DemovideoclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demovideoclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemovideoclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
