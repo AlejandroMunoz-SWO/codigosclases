e1b1ea12dcf3a6ff77169145f4b50d6fa0960abd
AlejandroSoftwareOne <114508877+AlejandroSoftwareOne@users.noreply.github.com>
2022-12-05 11:57:18 +0100

    Update DepartamentoController.java

--- servidor/empresa/src/main/java/com/example/empresa/controller/DepartamentoController.java
+++ servidor/empresa/src/main/java/com/example/empresa/controller/DepartamentoController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.GetExchange;
-
 import com.example.empresa.model.Departamento;
 import com.example.empresa.service.DepartametoService;
 
