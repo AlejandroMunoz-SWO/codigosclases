9d2796621540cc777d40f520dd856090017aa840
AlejandroSoftwareOne <ext.alejandro.munoz@softwareone.com>
2022-12-29 19:15:11 +0100

    FP DUAL SANTIAGO

---
+++ servidor/.net/ModelsAndControllers/ModelsAndControllers/Controllers/AutorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelsAndControllers.Models;
+
+namespace ModelsAndControllers.Controllers
+{
+    public class AutorController : Controller
+    {
+        [HttpGet("/autores")]
+        public ActionResult<List<Autor>>Get()
+        {
+            List<Autor> autores = new List<Autor>();
+
+            autores.Add(new Autor(1, "Camilo José Cela"));
+            autores.Add(new Autor(2, "Otero Pedrayo"));
+
+            return autores;
+
+        }
+    }
+}
---
+++ servidor/.net/ModelsAndControllers/ModelsAndControllers/Models/Autor.cs
@@ -0,0 +1,17 @@
+﻿namespace ModelsAndControllers.Models
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public Autor(int Id, string Nombre)
+        {
+            this.Id = Id;
+            this.Nombre = Nombre;
+
+        }
+
+
+    }
+}
